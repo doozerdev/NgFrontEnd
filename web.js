7b14cccb68952f029a1cbe154d8eeaa6e5898870
Joe Basirico <joebasirico@gmail.com>
2015-04-04 16:44:54 -0700

    fix web.js

--- web.js
+++ web.js
@@ -1,7 +1,11 @@
-var gzippo = require('gzippo');
+//var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
  
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+//app.use(express.logger('dev'));
+app.use("" + __dirname + "/dist");
+app.get('/', function(req, res) {
+    res.sendfile('index.html', {root: __dirname })
+});
+
+app.listen(process.env.PORT || 80);
