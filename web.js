21bea1f3a7529e3e4107cc7352b862c410046d3e
Joe Basirico <joebasirico@gmail.com>
2015-04-29 19:22:59 -0700

    heroku fix

--- web.js
+++ web.js
@@ -1,11 +1,7 @@
-//var gzippo = require('gzippo');
+var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
  
-//app.use(express.logger('dev'));
-app.use(express.static("" + __dirname + "/dist"));
-// app.get('/', function(req, res) {
-//     res.sendfile('index.html', {root: "" + __dirname + "/dist" })
-// });
-
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 80);
