4f1b9a5ba726749900dc115bc85d8686132d7801
Joe Basirico <joebasirico@gmail.com>
2015-07-18 16:17:39 -0700

    Build the damn thing

--- app/scripts/controllers/solution-interaction.js
+++ app/scripts/controllers/solution-interaction.js
@@ -2,6 +2,7 @@
 
 angular.module('webClientApp')
     .controller('SolutionInterCtrl', function($scope, $routeParams, Solution, Search, Item) {
+
         $scope.likeSolution = function(sol){
             Solution.like({
               id: sol.id,
