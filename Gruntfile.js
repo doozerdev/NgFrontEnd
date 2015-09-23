5b7fe1c3fd54e431e1b2dbeefd8dfa3930aea5bf
Rebecca Deutsch <rdeutsch@gmail.com>
2015-09-22 19:15:45 -0700

    fixes #33 - grunt build the directive html files

--- Gruntfile.js
+++ Gruntfile.js
@@ -265,13 +265,14 @@ module.exports = function (grunt) {
 
     // Performs rewrites based on filerev and the useminPrepare configuration
     usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}*.html'],
+      html: ['<%= yeoman.dist %>/**/*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
       options: {
         assetsDirs: [
           '<%= yeoman.dist %>',
           '<%= yeoman.dist %>/images',
-          '<%= yeoman.dist %>/styles'
+          '<%= yeoman.dist %>/styles',
+          '<%= yeoman.dist %>/icons'
         ]
       }
     },
@@ -336,7 +337,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.dist %>',
-          src: ['*.html', 'views/{,*/}*.html', 'scripts/directives/{,*/}*.html'],
+          src: ['*.html', 'views/{,*/}*.html', 'scripts/directives/*.html'],
           dest: '<%= yeoman.dist %>'
         }]
       }
@@ -375,6 +376,7 @@ module.exports = function (grunt) {
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
+            'scripts/directives/*.html',
             'images/{,*/}*.{webp}',
             'icons/{,*/}*.{png,ico,json}',
             'styles/fonts/{,*/}*.*'
